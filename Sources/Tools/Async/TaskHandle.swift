9aa15acfdda662312f56f30c8d1cd3a2d81a2c1b
adamz <adam@zethrae.us>
2026-02-28 16:22:30 -0800

    taskholder

--- Sources/Tools/Async/TaskHandle.swift
+++ Sources/Tools/Async/TaskHandle.swift
@@ -1,65 +1,20 @@
-import Observation
-
-/// A property wrapper which holds one `Task<Value, Never>` and cancels
+/// A property wrapper which holds one `Task<Success, Failure>` and cancels
 /// it when a new value is set, or when it is deallocated.
-@propertyWrapper
-public struct TaskHandle<Value: Sendable>: Sendable {
-
-  final class Box: Sendable {
-    let value = Mut<Task<Value, Never>?>(nil)
-    deinit {
-      value.withLock { value in
-        value
-      }?
-      .cancel()
-    }
-  }
-
+@propertyWrapper public struct TaskHolder<Success: Sendable, Failure: Error>: ~Copyable {
   public init(
-    _ type: Value.Type = Value.self,
-    wrappedValue: Task<Value, Never>? = nil
+    success: Success.Type = Success.self, failure: Failure.Type = Failure.self,
+    wrappedValue: Task<Success, Failure>? = nil
   ) {
     self.wrappedValue = wrappedValue
   }
 
-  private let box: Box = .init()
-  private var boxedValue: Task<Value, Never>? {
-    get {
-      box.value.withLock { $0 }
-    }
-    nonmutating set {
-      box.value.withLock { value in
-        value = newValue
-      }
-    }
-  }
-  public var wrappedValue: Task<Value, Never>? {
-    get {
-      boxedValue
-    }
-    nonmutating set {
-      if let boxedValue, boxedValue != newValue {
-        boxedValue.cancel()
-      }
-      boxedValue = newValue
-    }
-  }
-  public var projectedValue: TaskBuilder<Value> {
-    TaskBuilder<Value> {
-      wrappedValue = $0
-    }
-  }
-}
-
-public struct TaskBuilder<Value: Sendable>: Sendable {
-
-  let sink: @Sendable (Task<Value, Never>) -> Void
-
-  public func start(
-    priority: TaskPriority? = nil,
-    operation: sending @escaping @isolated(any) () async -> Value
-  ) {
-    sink(Task<Value, Never>(priority: priority, operation: operation))
+  deinit {
+    wrappedValue?.cancel()
   }
 
+  public var wrappedValue: Task<Success, Failure>? = nil {
+    willSet {
+      wrappedValue?.cancel()
+    }
+  }
 }
