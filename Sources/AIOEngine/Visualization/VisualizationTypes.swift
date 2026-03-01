8c90636561c184b0b2ccd5758065114f38839cc6
adamz <adam@zethrae.us>
2026-02-28 22:36:13 -0800

    factor views out of AVC

--- Sources/AIOEngine/Visualization/VisualizationTypes.swift
+++ Sources/AIOEngine/Visualization/VisualizationTypes.swift
@@ -1,5 +1,5 @@
 #if canImport(AVFAudio)
-  import Foundation
+  public import Foundation
   public import AudioSignals
 
   // MARK: - Visualization Events & Subscription
@@ -14,12 +14,44 @@
     case latestBufferTiming(BufferTiming?)
   }
 
+  /// Per-subscriber filter for visualization event delivery.
+  @safe public struct VisualizationEventMask: OptionSet, Sendable, Equatable, Hashable, Codable {
+    public let rawValue: UInt8
+
+    public init(rawValue: UInt8) {
+      self.rawValue = rawValue
+    }
+
+    public static let lodSnapshot = Self(rawValue: 1 << 0)
+    public static let lodSnapshotBackground = Self(rawValue: 1 << 1)
+    public static let timeDomain = Self(rawValue: 1 << 2)
+    public static let frequencyDomain = Self(rawValue: 1 << 3)
+    public static let beat = Self(rawValue: 1 << 4)
+    public static let latestBufferTiming = Self(rawValue: 1 << 5)
+
+    public static let all: Self = [
+      .lodSnapshot,
+      .lodSnapshotBackground,
+      .timeDomain,
+      .frequencyDomain,
+      .beat,
+      .latestBufferTiming,
+    ]
+
+    public static let none: Self = []
+  }
+
   /// Work request submitted by a visualization subscriber.
   @safe public struct VisualizationRequest: Sendable, Equatable {
     public var work: VisualizationWork
+    public var eventMask: VisualizationEventMask
 
-    public init(work: VisualizationWork = .none) {
+    public init(
+      work: VisualizationWork = .none,
+      eventMask: VisualizationEventMask = .all
+    ) {
       self.work = work
+      self.eventMask = eventMask
     }
   }
 
@@ -35,7 +67,7 @@
     private var isCancelled = false
     private let cancelHandler: @MainActor () -> Void
 
-    init(cancelHandler: @escaping @MainActor () -> Void) {
+    public init(cancelHandler: @escaping @MainActor () -> Void) {
       self.cancelHandler = cancelHandler
     }
 
@@ -44,6 +76,21 @@
       isCancelled = true
       cancelHandler()
     }
+  }
+
+  /// Minimal protocol surface needed by external waveform processing consumers.
+  public protocol VisualizationDriving: AnyObject, Sendable {
+    nonisolated var currentSampleRate: Double { get }
+    nonisolated var currentTimeSeconds: TimeInterval { get }
+
+    @MainActor
+    func subscribe(
+      request: VisualizationRequest,
+      handler: @escaping @Sendable (VisualizationEvent) -> Void
+    ) -> VisualizationSubscription
+
+    nonisolated
+      func withCurrentLODSnapshotRef<R>(_ body: (LODSnapshotRef) -> R) -> R?
   }
 
 #endif
